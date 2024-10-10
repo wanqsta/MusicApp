7725dbcf148ed6fbc6267f5e771b6e92300a7cce
talantov2704 <talantov2704@gmail.com>
2024-10-09 21:17:49 -0400

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Urmat Talantov on 10/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MusicAppTests/MusicAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppTests.swift
+//  MusicAppTests
+//
+//  Created by Urmat Talantov on 10/9/24.
+//
+
+import Testing
+@testable import MusicApp
+
+struct MusicAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
